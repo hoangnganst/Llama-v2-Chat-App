8d3528111ecd77d63e5783fc2a65be72cb73f4ca
Kevin Knights <kevinknights29@hotmail.com>
2023-07-29 23:39:37 -0500

    updated .devcontainer files

--- app/.devcontainer/install_dependencies.sh
+++ app/.devcontainer/install_dependencies.sh
@@ -6,9 +6,6 @@ if [ -z "$1" ]; then
     exit 1
 fi
 
-# Set the path of the requirements.txt file
-REQUIREMENTS_FILE=$1
-
 # Installing prerequisites
 apt-get update && \
     apt-get install -y \
@@ -25,6 +22,9 @@ python3 -m venv .venv
 
 # Activate the virtual environment
 source .venv/bin/activate
+
+# Set the path of the requirements.txt file
+REQUIREMENTS_FILE=$1
 
 # Install the Python packages
 pip3 install -r $REQUIREMENTS_FILE
